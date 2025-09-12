2fde1a3c090d537cd01ae0e6acc1843ea65e8413
sinyeyoung <s97863395@gmail.com>
2025-09-12 11:59:40 +0900

    Add arithmetic operations

---
+++ test1.java
@@ -0,0 +1,18 @@
+public class test1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a % b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+       // System.out.println("product: " + product);
+        //System.out.println("quotient: " + quotient);
+    }
+}
+
+
