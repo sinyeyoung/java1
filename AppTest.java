85fabc879104d36d597ce9dfbe7618b20c7e8994
sinyeyoung <134347961+sinyeyoung@users.noreply.github.com>
2025-09-19 12:27:31 +0900

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testHello(){
+        assertEquals("Hello,World", App.sayHello());
+    }
+}
